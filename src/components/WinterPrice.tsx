5cc3aa133af6a16ad221b8b518cf04c4df776fde
LeylaKhb <leyla.khbi@gmail.com>
2025-09-14 17:12:32 +0300

    обновила цены

--- src/components/WinterPrice.tsx
+++ src/components/WinterPrice.tsx
@@ -102,10 +102,10 @@ const WinterPrice = () => {
                         <span style={{fontWeight: 600}}>7000₽</span> <span>1 час</span>
                     </div>
                     <div className="price-row">
-                        <span style={{fontWeight: 600}}>12000₽</span> <span>2 часа</span>
+                        <span style={{fontWeight: 600}}>11000₽</span> <span>2 часа</span>
                     </div>
                     <div className="price-row">
-                        <span style={{fontWeight: 600}}>16000₽</span> <span>3 часа</span>
+                        <span style={{fontWeight: 600}}>15000₽</span> <span>3 часа</span>
                     </div>
                 </div>
                 <div style={{width: '90%', marginRight: '5%', marginLeft: '5%', marginTop: 20, boxSizing: "border-box"}}>
